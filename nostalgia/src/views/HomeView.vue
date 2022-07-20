b6d0450fa3e9cb12923f78155c29f40453cc0141
nsktky <nosaka.takuya11@gmail.com>
2022-07-20 22:14:00 +0900

    Homeの見た目

--- nostalgia/src/views/HomeView.vue
+++ nostalgia/src/views/HomeView.vue
@@ -16,25 +16,88 @@ export default {
     "vue-p5": VueP5
   },
   data: () => ({
+     points : [],
+     bar : [],
+     barSize : [],
+     bars: 0
   }),
-  computed: {
-  },
   methods: {
     setup(sketch) {
       sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
-      sketch.background(0);
+      this.bars = sketch.int(sketch.random(3, 6));
+      sketch.background(85, 0, 48);
+      sketch.noiseDetail(4);
+      sketch.angleMode(sketch.DEGREES);
+
+      let tileCount = 40;
+      let grid = sketch.width / tileCount;
+
+      for (let y = 0; y <= sketch.height; y += grid) {
+        for (let x = 0; x <= sketch.width; x += grid) {
+          let p = sketch.createVector(x, y);
+          this.points.push(p);
+        }
+      }
+
+      for (let i = 0; i < this.bars; i++) {
+        let x = sketch.random(sketch.width*0.2, sketch.width*0.8);
+        let y = sketch.random(sketch.height*0.2, sketch.height*0.8);
+        let p = sketch.createVector(x, y);
+        let size = sketch.random(sketch.width*0.01, sketch.width*0.1);
+        this.bar.push(p);
+        this.barSize.push(size);
+      }
+
+      sketch.textAlign(sketch.CENTER, sketch.CENTER);
+      sketch.textSize(sketch.width * 0.15);
     },
+
     draw(sketch) {
-      sketch.push();
-      sketch.translate(sketch.width/2,sketch.height/sketch.random(1,5));
-      sketch.rotate(sketch.map(sketch.random(sketch.frameCount),0,1,0,360));
-      sketch.stroke(207, 168, 85, 30);
-      sketch.noFill();
-      sketch.rect(0, 0, sketch.width*0.1, sketch.width*0.1);
-      sketch.line(0, 0, sketch.width, sketch.height);
-      sketch.pop();
-      sketch.stroke(255);
-      sketch.text('nostalgia', sketch.width/2, sketch.height/2)
+      sketch.noStroke();
+      for (let i = 0; i < this.points.length; i++) {
+        let angle = sketch.map(
+          sketch.noise(this.points[i].x * 0.001, this.points[i].y * 0.001), 0, 1, 100, 600);
+          this.points[i].add(sketch.createVector(sketch.sin(angle), sketch.cos(angle)));
+
+        if (this.points[i].x > sketch.width) this.points[i].x = sketch.random(sketch.width);
+        if (this.points[i].x < 0) this.points[i].x = sketch.random(sketch.width);
+        if (this.points[i].y < 0) this.points[i].y = sketch.random(sketch.height);
+        if (this.points[i].y > sketch.height) this.points[i].y = sketch.random(sketch.height);
+
+        sketch.push();
+
+        const gradientFill = sketch.drawingContext.createRadialGradient(
+          sketch.width * 0.5,
+          sketch.height * 0.5,
+          sketch.width * 0.1,
+          sketch.width *0.5,
+          sketch.height * 0.5,
+          sketch.width * 0.7
+        );
+        gradientFill.addColorStop(0, sketch.color(252, 232, 225));
+        gradientFill.addColorStop(0.5, sketch.color(200, 162, 142));
+        gradientFill.addColorStop(1, sketch.color(178, 120, 85));
+        sketch.drawingContext.fillStyle = gradientFill;
+
+        sketch.circle(this.points[i].x+sketch.random(5), this.points[i].y+sketch.random(5), 5);
+
+        sketch.pop();
+      }
+
+      for(let i = 0; i < this.bar.length; i++) {
+        sketch.strokeWeight(sketch.min(sketch.width ,sketch.height)*0.03);
+        sketch.stroke(0, 200);
+        sketch.line(this.bar[i].x, this.bar[i].y,
+          this.bar[i].x + this.barSize[i] * sketch.frameCount * 0.005,
+          this.bar[i].y - this.barSize[i] * sketch.frameCount * 0.005);
+        sketch.stroke(250);
+        sketch.line(this.bar[i].x, this.bar[i].y,
+          this.bar[i].x, this.bar[i].y - this.barSize[i]);
+      }
+
+      sketch.noStroke();
+      sketch.fill(0);
+      sketch.text('NOSTALGIA', sketch.width/2, sketch.height/2)
     },
   }
 };
