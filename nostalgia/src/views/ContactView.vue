aa55e7a25f54c64435f5cfe5274d1f0cc4fd0213
nsktky <nosaka.takuya11@gmail.com>
2022-07-23 13:12:03 +0900

    contactの作成

--- nostalgia/src/views/ContactView.vue
+++ nostalgia/src/views/ContactView.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    <h1>Contact Page</h1>
-  </div>
+  <v-app id="inspire">
+    <v-main>
+      <v-container class="container">
+        <v-row justify="center" align-content="center">
+          <v-col sm="8">
+            <v-card  height="80vh" rounded>
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSelZPOladVnrM6zbTRUoakFWNGa3M5HTQ1-WGS_a60QLVZr3g/viewform?embedded=true"
+                 width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+            </v-card>
+           </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
+<style>
+.container {
+    padding-top: 2rem;
+}
+</style>
